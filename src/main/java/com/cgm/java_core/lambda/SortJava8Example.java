23d631b30529c97e3cba64b1e84d75089663123d
CGM <mrmouse.guru@gmail.com>
2024-02-28 10:07:14 +0700

    Sorting use lambda expression in Java8

---
+++ src/main/java/com/cgm/java_core/lambda/SortJava8Example.java
@@ -0,0 +1,28 @@
+package com.cgm.java_core.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortJava8Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Một danh sách các loại quả.
+				List<String> fruits = Arrays.asList("Grapefruit", "Apple", "Durian", "Cherry");
+
+				// Sử dụng phương thức tiện ích của Collections 
+				// để sắp xếp lại danh sách trên.
+				// Cung cấp một Comparator (Bộ so sách).
+				Collections.sort(fruits, 
+						(o1, o2 ) -> {return o1.compareTo(o2);});
+
+				for (String fruit : fruits) {
+					System.out.println(fruit);
+				}
+		
+
+	}
+
+}
